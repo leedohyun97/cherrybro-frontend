4389336bee429e76fcf9fd71fda17eea9dd6577f
zszz9709 <zszz9709@gmail.com>
2025-05-07 10:51:19 +0900

    feat : app.js 최초 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import UsersRegisterPage from './pages/UsersRegisterPage';
+import UsersLoginPage from './pages/UsersLoginPage';
+import ChickEntryPage from './pages/ChickEntryPage';
+import ChickDeathPage from './pages/ChickDeathPage';
+import FarmSectionPage from './pages/FarmSectionPage';
+import { AuthProvider } from './util/authContext';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <AuthProvider>
+    <Router>
+      <div className="app-container">
+        <header className="app-header">
+          CHERRYBRO
+        </header>
+        <div className="app-body">
+          <Sidebar />
+          <main className="app-main">
+            <Routes>
+              <Route path="/login" element={<UsersLoginPage />} />
+              <Route path="/register" element={<UsersRegisterPage />} />
+              <Route path="/chick-entry" element={<ChickEntryPage />} />
+              <Route path="/chick-death" element={<ChickDeathPage />} />
+              <Route path="/farm-section" element={<FarmSectionPage />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  </AuthProvider>
   );
 }
-
-export default App;

6a3e04851f960fb23a4db00a5f2ee8bd1506bb38
zszz9709 <zszz9709@gmail.com>
2025-05-12 16:56:37 +0900

    refactor : layout 나눔눔

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import UsersRegisterPage from './pages/UsersRegisterPage';
+import MainPage from './pages/MainPage';
 import UsersLoginPage from './pages/UsersLoginPage';
+import UsersRegisterPage from './pages/UsersRegisterPage';
 import ChickEntryPage from './pages/ChickEntryPage';
 import ChickDeathPage from './pages/ChickDeathPage';
+import ChickDisposalPage from './pages/ChickDisposalPage';
 import FarmSectionPage from './pages/FarmSectionPage';
+import AdminFarmSectionPage from './pages/AdminFarmSectionPage';
+import MainLayout from './layouts/MainLayout';
+import PublicLayout from './layouts/PublicLayout';
 import { AuthProvider } from './util/authContext';
+
 import './App.css';
 
 export default function App() {
   return (
   <AuthProvider>
     <Router>
-      <div className="app-container">
-        <header className="app-header">
-          CHERRYBRO
-        </header>
-        <div className="app-body">
-          <Sidebar />
-          <main className="app-main">
-            <Routes>
-              <Route path="/login" element={<UsersLoginPage />} />
-              <Route path="/register" element={<UsersRegisterPage />} />
-              <Route path="/chick-entry" element={<ChickEntryPage />} />
-              <Route path="/chick-death" element={<ChickDeathPage />} />
-              <Route path="/farm-section" element={<FarmSectionPage />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
+      <Routes>
+        {/* ---------- PublicLayout : 헤더/사이드바 없음 ---------- */}
+        <Route element={<PublicLayout />}>
+          <Route index path="/login" element={<UsersLoginPage />} />
+          <Route path="/register" element={<UsersRegisterPage />} />
+        </Route>
+
+          {/* ---------- MainLayout : 헤더/사이드바 있음 ---------- */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/chick-entry" element={<ChickEntryPage />} />
+            <Route path="/chick-death" element={<ChickDeathPage />} />
+            <Route path="/chick-disposal" element={<ChickDisposalPage />} />
+            <Route path="/farm-section" element={<FarmSectionPage />} />
+            <Route path="/admin-farm-section" element={<AdminFarmSectionPage />} />
+          </Route>
+      </Routes>
     </Router>
   </AuthProvider>
   );

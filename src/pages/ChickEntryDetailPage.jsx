3a2bf7afbee95af244197da92b5b245521bdf898
zszz9709 <zszz9709@gmail.com>
2025-05-26 13:55:25 +0900

    refactor : 입추 내역 조회 시 오류 해결 -> 백엔드 API 새로 제작(농장 번호로 입추 리스트 조회) 후 사용용

--- src/pages/ChickEntryDetailPage.jsx
+++ src/pages/ChickEntryDetailPage.jsx
@@ -27,7 +27,7 @@ export default function ChickEntryDetailPage() {
   const [chickEntry, setChickEntry] = useState([]); 
 
   //필터링 시 선택한 농장 번호
-  const [selectedFarmNo, setSelectedFarmNo] = useState('');
+  const [selectedFarmNo, setSelectedFarmNo] = useState(null);
 
   //필터링 시 선택한 농장 번호
   const [currentPage, setCurrentPage] = useState(0);
@@ -52,6 +52,8 @@ export default function ChickEntryDetailPage() {
 
   useEffect(() => {
 
+      if (!token) return;
+      
       //선택 농장이 바뀌면 페이지는 1페이지로 초기화
       setCurrentPage(0);
       
@@ -59,16 +61,17 @@ export default function ChickEntryDetailPage() {
           try {
               const [getAllFarm, getAllFarmSection, getAllChickEntry] = await Promise.all([
                   farmApi.getAllFarm(token),
-                  selectedFarmNo ? farmSectionApi.getAllFarmSectionByFarmNo(selectedFarmNo) : farmSectionApi.getAllFarmSection(),
-                  chickEntryApi.getAllChickEntries()
+                  farmSectionApi.getAllFarmSection(),
+                  selectedFarmNo !== null ? chickEntryApi.getChickEntriesByFarmNo(selectedFarmNo) : chickEntryApi.getAllChickEntries()
               ]);
 
               const chickEntrySort = [...getAllChickEntry.data]
-                  .sort((a, b) => new Date(b.chickEntryCreateAt) - new Date(a.chickEntryCreateAt)); // 병아리 입추 정보를 등록 날짜 기준으로 내림차순 정렬
+              .sort((a, b) => new Date(b.chickEntryCreateAt) - new Date(a.chickEntryCreateAt)); // 병아리 입추 정보를 등록 날짜 기준으로 내림차순 정렬
               
               setFarm(getAllFarm.data); // 농장 정보를 상태에 저장
               setFarmSection(getAllFarmSection.data); // 농장 구역 정보를 상태에 저장
               setChickEntry(chickEntrySort); // 병아리 입고 정보를 상태에 저장
+              
 
           } catch (error) {
               console.error("Error fetching data:", error); // 에러를 콘솔에 출력한다
@@ -86,6 +89,10 @@ export default function ChickEntryDetailPage() {
     currentPage * PAGE_SIZE, (currentPage+1) * PAGE_SIZE
   );
 
+  const handleChangeNumber = (e) => {
+    const value = e.target.value;
+    setSelectedFarmNo(value ? Number(value) : null);
+  }
 
   return (
     <div className="admin-page">
@@ -97,7 +104,7 @@ export default function ChickEntryDetailPage() {
         <select
           id="farm-select"
           value={selectedFarmNo}
-          onChange={(e) => setSelectedFarmNo(e.target.value)}
+          onChange={handleChangeNumber}
         >
           <option value="">전체 보기</option>
           {/* 농장 배열의 이름 전체 출력, key=농장번호 value=농장번호 */}
@@ -116,7 +123,7 @@ export default function ChickEntryDetailPage() {
                 <th>동</th>
                 <th>입추일</th>
                 <th>입추 등록일</th>
-                <th>수량</th>
+                <th>입추 수</th>
             <th/>
           </tr>
         </thead>

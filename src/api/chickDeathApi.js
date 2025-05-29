763992d55fe36499bcd053963ce83f59aec41e2d
zszz9709 <zszz9709@gmail.com>
2025-05-29 09:18:03 +0900

    chore : 줄바꿈 수정

--- src/api/chickDeathApi.js
+++ src/api/chickDeathApi.js
@@ -1,16 +1,16 @@
-import API from './axiosInstance';
+import API from "./axiosInstance";
 
 //폐사 등록
 export const createChickDeath = async (chickDeathDto) => {
   try {
-    console.log('Request:', chickDeathDto);
+    console.log("Request:", chickDeathDto);
 
-    const { data } = await API.post('/chickDeath', chickDeathDto);
+    const { data } = await API.post("/chickDeath", chickDeathDto);
 
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('createChickDeath error:', err);
+    console.error("createChickDeath error:", err);
     // 필요하면 사용자용 메시지 가공 후 throw
     throw err;
   }
@@ -19,27 +19,28 @@ export const createChickDeath = async (chickDeathDto) => {
 //폐사 수정
 export const updateChickDeath = async (chickDeathNo, chickDeathDto) => {
   try {
-    const { data } = await API.put(`/chickDeath/${chickDeathNo}`, chickDeathDto);
+    const { data } = await API.put(
+      `/chickDeath/${chickDeathNo}`,
+      chickDeathDto
+    );
     return data;
   } catch (e) {
-    console.error('updateChickDeath error:', e);
+    console.error("updateChickDeath error:", e);
     throw e;
   }
 };
 
-
 //폐사 삭제
 export const deleteChickDeath = async (chickDeathNo) => {
   try {
-
-    console.log('Request:', chickDeathNo);
+    console.log("Request:", chickDeathNo);
 
     const { data } = await API.delete(`/chickDeath/${chickDeathNo}`);
 
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('deleteChickDeath error:', err);
+    console.error("deleteChickDeath error:", err);
     throw err;
   }
 };
@@ -47,15 +48,14 @@ export const deleteChickDeath = async (chickDeathNo) => {
 //폐사 조회
 export const getChickDeath = async (chickDeathNo) => {
   try {
-
-    console.log('Request:', chickDeathNo);
+    console.log("Request:", chickDeathNo);
 
     const { data } = await API.get(`/chickDeath/${chickDeathNo}`);
 
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('getChickDeath error:', err);
+    console.error("getChickDeath error:", err);
     // 필요하면 사용자용 메시지 가공 후 throw
     throw err;
   }
@@ -64,15 +64,16 @@ export const getChickDeath = async (chickDeathNo) => {
 //폐사 목록 조회
 export const getChickDeathByFarmSectionNo = async (farmSectionNo) => {
   try {
-    console.log('Request:', farmSectionNo);
+    console.log("Request:", farmSectionNo);
 
-    const { data } = await API.get(`/chickDeath/list?farmSectionNo=${farmSectionNo}`);
-    console.log('Response:', data);
+    const { data } = await API.get(
+      `/chickDeath/list?farmSectionNo=${farmSectionNo}`
+    );
+    console.log("Response:", data);
 
     return data;
-    
   } catch (err) {
-    console.error('getChickDeathByFarmSectionNo error:', err);
+    console.error("getChickDeathByFarmSectionNo error:", err);
     throw err;
   }
 };
@@ -81,26 +82,28 @@ export const getChickDeathByFarmSectionNo = async (farmSectionNo) => {
 export const getAllChickDeath = async () => {
   try {
     const { data } = await API.get(`/chickDeath/list/all`);
-    console.log('Response:', data);
+    console.log("Response:", data);
 
     return data;
-    
   } catch (err) {
-    console.error('getAllChickDeath error:', err);
+    console.error("getAllChickDeath error:", err);
     throw err;
   }
 };
 
 //농장동 폐사 수 누적합 조회
-export const getTotalChickDeathNumberByFarmSectionNo = async (farmSectionNo) => {
+export const getTotalChickDeathNumberByFarmSectionNo = async (
+  farmSectionNo
+) => {
   try {
-    const { data } = await API.get(`/chickDeath/list/total?farmSectionNo=${farmSectionNo}`);
-    console.log('Response:', data);
+    const { data } = await API.get(
+      `/chickDeath/list/total?farmSectionNo=${farmSectionNo}`
+    );
+    console.log("Response:", data);
 
     return data;
-
   } catch (err) {
-    console.error('getTotalChickDeathNumberByFarmSectionNo error:', err);
+    console.error("getTotalChickDeathNumberByFarmSectionNo error:", err);
 
     throw err;
   }
--- src/api/farmApi.js
+++ src/api/farmApi.js
@@ -1,16 +1,16 @@
-import API from './axiosInstance';
+import API from "./axiosInstance";
 
 //농장 등록
 export const createFarm = async (FarmDto) => {
   try {
-    console.log('Request:', FarmDto);
+    console.log("Request:", FarmDto);
 
-    const { data } = await API.post('/farm', FarmDto);
+    const { data } = await API.post("/farm", FarmDto);
 
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('createFarm error:', err);
+    console.error("createFarm error:", err);
     throw err;
   }
 };
@@ -21,40 +21,41 @@ export const updateFarm = async (FarmNo, FarmDto) => {
     const { data } = await API.put(`/farm/${FarmNo}`, FarmDto);
     return data;
   } catch (err) {
-    console.error('updateFarm error:', err);
+    console.error("updateFarm error:", err);
     throw err;
   }
 };
-
 
 //농장 삭제
 export const deleteFarm = async (FarmNo) => {
   try {
-
-    console.log('Request:', FarmNo);
+    console.log("Request:", FarmNo);
 
     const { data } = await API.delete(`/farm/${FarmNo}`);
 
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('deleteFarm error:', err);
+    console.error("deleteFarm error:", err);
     throw err;
   }
 };
 
-//농장 조회
-export const getFarm = async (FarmNo) => {
+//농장 번호로 농장 조회(관리자)
+export const getFarmByFarmNo = async (token, FarmNo) => {
   try {
+    console.log("Request:", FarmNo);
 
-    console.log('Request:', FarmNo);
+    const { data } = await API.get(`/farm/by-farmNo/${FarmNo}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    const { data } = await API.get(`/farm/${FarmNo}`);
-
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('getFarm error:', err);
+    console.error("getFarm error:", err);
     throw err;
   }
 };
@@ -62,16 +63,15 @@ export const getFarm = async (FarmNo) => {
 //사용자 번호로 농장 조회(농장주)
 export const getMyFarm = async (token) => {
   try {
-
     const { data } = await API.get(`/farm/my-farm`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('getFarm error:', err);
+    console.error("getFarm error:", err);
     throw err;
   }
 };
@@ -79,16 +79,15 @@ export const getMyFarm = async (token) => {
 //사용자 번호로 농장 조회(관리자)
 export const getFarmByUsersNo = async (token, usersNo) => {
   try {
-
     const { data } = await API.get(`/farm/${usersNo}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log('Response:', data);
+    console.log("Response:", data);
     return data;
   } catch (err) {
-    console.error('getFarm error:', err);
+    console.error("getFarm error:", err);
     throw err;
   }
 };
@@ -96,18 +95,17 @@ export const getFarmByUsersNo = async (token, usersNo) => {
 //농장 목록 조회
 export const getAllFarm = async (token) => {
   try {
-    const { data } = await API.get('/farm/list', {
+    const { data } = await API.get("/farm/list", {
       headers: {
         Authorization: `Bearer ${token}`,
-      }
+      },
     });
 
-    console.log('Response:', data);
+    console.log("Response:", data);
 
     return data;
-
   } catch (err) {
-    console.error('getAllFarm error:', err);
+    console.error("getAllFarm error:", err);
     throw err;
   }
 };

c2f83d4b84a8af47c42169862edac9fb61d04dde
zszz9709 <zszz9709@gmail.com>
2025-06-04 13:38:34 +0900

    refactor : 도태폐사 => chickReduce페이지 하나로 수정

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,18 @@
-import '../styles/Sidebar.css';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useUsersAuth } from '../util/authContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from '../images/체리부로.png';
-import user_icon from '../images/user_icon.png';
+import "../styles/Sidebar.css";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useUsersAuth } from "../util/authContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../images/체리부로.png";
+import user_icon from "../images/user_icon.png";
 import {
   faHouse,
   faPenToSquare,
   faRightFromBracket,
   faRightToBracket,
   faUserPlus,
-  faClockRotateLeft 
-} from '@fortawesome/free-solid-svg-icons';
+  faClockRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
   const { token, logout, users } = useUsersAuth();
@@ -22,19 +22,19 @@ export default function Sidebar() {
 
   const handleLogout = () => {
     logout();
-    navigate('/main');
+    navigate("/main");
   };
 
   return (
     <>
       <button
-        className={`hamburger ${sidebarOpen ? 'white' : 'black'}`}
+        className={`hamburger ${sidebarOpen ? "white" : "black"}`}
         onClick={() => setSidebarOpen(!sidebarOpen)}
       >
         ☰
       </button>
 
-      <nav className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+      <nav className={`sidebar ${sidebarOpen ? "open" : ""}`}>
         {/* 로고 */}
         <div className="sidebar-logo">
           <img src={logo} alt="Cherrybro 로고" />
@@ -47,7 +47,7 @@ export default function Sidebar() {
             <div className="profile-text">
               <div className="profile-name">{users.usersName}</div>
               <div className="profile-role">
-                {usersRole === 'ROLE_ADMIN' ? '관리자' : '농장주'}
+                {usersRole === "ROLE_ADMIN" ? "관리자" : "농장주"}
               </div>
             </div>
           </div>
@@ -57,58 +57,73 @@ export default function Sidebar() {
         <ul className="sidebar-menu">
           {token ? (
             <>
-              {usersRole === 'ROLE_ADMIN' && (
+              {usersRole === "ROLE_ADMIN" && (
                 <>
                   <li>
                     <Link to="/admin-farm-section">
-                      <FontAwesomeIcon icon={faHouse} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faHouse}
+                        className="sidebar-icon"
+                      />
                       메인
                     </Link>
                   </li>
                   <li>
                     <Link to="/chick-entry">
-                      <FontAwesomeIcon icon={faPenToSquare} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faPenToSquare}
+                        className="sidebar-icon"
+                      />
                       입추 등록
                     </Link>
                   </li>
                   <li>
                     <Link to="/chick-entry/detail">
-                      <FontAwesomeIcon icon={faClockRotateLeft} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faClockRotateLeft}
+                        className="sidebar-icon"
+                      />
                       입추 내역
                     </Link>
                   </li>
                 </>
               )}
 
-              {usersRole === 'ROLE_FARMER' && (
+              {usersRole === "ROLE_FARMER" && (
                 <>
                   <li>
                     <Link to="/farm-section">
-                      <FontAwesomeIcon icon={faHouse} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faHouse}
+                        className="sidebar-icon"
+                      />
                       메인
                     </Link>
                   </li>
                   <li>
-                    <Link to="/chick-disposal">
-                      <FontAwesomeIcon icon={faPenToSquare} className="sidebar-icon" />
-                      도사 등록
+                    <Link to="/chick-reduce">
+                      <FontAwesomeIcon
+                        icon={faPenToSquare}
+                        className="sidebar-icon"
+                      />
+                      도태/폐사 등록
                     </Link>
                   </li>
                   <li>
                     <Link to="/chick-disposal/detail">
-                      <FontAwesomeIcon icon={faClockRotateLeft} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faClockRotateLeft}
+                        className="sidebar-icon"
+                      />
                       도사 내역
                     </Link>
                   </li>
                   <li>
-                    <Link to="/chick-death">
-                      <FontAwesomeIcon icon={faPenToSquare} className="sidebar-icon" />
-                      폐사 등록
-                    </Link>
-                  </li>
-                  <li>
                     <Link to="/chick-death/detail">
-                      <FontAwesomeIcon icon={faClockRotateLeft} className="sidebar-icon" />
+                      <FontAwesomeIcon
+                        icon={faClockRotateLeft}
+                        className="sidebar-icon"
+                      />
                       폐사 내역
                     </Link>
                   </li>
@@ -117,7 +132,10 @@ export default function Sidebar() {
 
               <li>
                 <button onClick={handleLogout} className="logout-link">
-                  <FontAwesomeIcon icon={faRightFromBracket} className="sidebar-icon" />
+                  <FontAwesomeIcon
+                    icon={faRightFromBracket}
+                    className="sidebar-icon"
+                  />
                   로그아웃
                 </button>
               </li>
@@ -126,7 +144,10 @@ export default function Sidebar() {
             <>
               <li>
                 <Link to="/login">
-                  <FontAwesomeIcon icon={faRightToBracket} className="sidebar-icon" />
+                  <FontAwesomeIcon
+                    icon={faRightToBracket}
+                    className="sidebar-icon"
+                  />
                   로그인
                 </Link>
               </li>
